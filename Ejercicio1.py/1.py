d3ab190e0b7c4eda43052c0d2c2a1a73f016975c
Luciagase <113980773+Luciagase@users.noreply.github.com>
2023-03-29 22:08:47 +0200

    correción

--- Ejercicio1.py/1.py
+++ Ejercicio1.py/1.py
@@ -13,6 +13,7 @@ class Torres:
         print("Torre 1:", self.torre1)
         print("Torre 2:", self.torre2)
         print("Torre 3:", self.torre3)
+        self._resolver(self.n, self.torre1, self.torre2, self.torre3)#llama a la función recursiva _resolver
 
     def _resolver(self, n, origen, auxiliar, destino):
         if n == 1:#solo haya un disco en la torre de origen, se mueve directamente a la torre de destino y se imprime el movimiento
@@ -29,5 +30,5 @@ class Torres:
             print("Torre 2:", self.torre2)
             print("Torre 3:", self.torre3)
             self._resolver(n-1, auxiliar, origen, destino)#vuelve a llamar a _resolver para mover los discos de la torre auxiliar a la torre de destino.
-            
+
     
